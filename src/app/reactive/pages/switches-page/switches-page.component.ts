220f3f39950d792cd0211a435080563da9b618f9
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2023-08-11 18:29:24 -0400

    switches

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class SwitchesPageComponent {
 
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    WantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue]
+  });
+
+  public person = {
+
+  }
+
+  constructor( private fb: FormBuilder ){}
+
+
+  isValidField( field: string ): boolean | null {
+    return this.myForm.controls[field].errors
+    && this.myForm.controls[field].touched;
+  }
+
+  onSave(): void {
+    if ( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+  }
 }
